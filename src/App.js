00c02b071ed96e072d5f17ca0d90b7f24537ac74
Tom <tom.pastor.etu@univ-lemans.fr>
2020-11-25 16:06:58 +0100

    Ajout appels API : récupération des films
    -Appel de l'API TheMovieDB et affichage des 20 films les plus populaires sous forme textuelle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+// Informations API
+const API_KEY = '92b418e837b833be308bbfb1fb2aca1e';
+const API_DISCOVER_QUERY = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=frFR&sort_by=popularity.desc&page=1&timezone=Europe/Paris&include_null_first_air_dates=false`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_DISCOVER_QUERY)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setIsLoading(false);
+          setMovies(result.results);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if(error) {
+    return <p> Erreur de chargement </p>;
+  }else if(isLoading) {
+    return <p> Chargement... </p>;
+  }else{
+    return (
+      <div>
+        {movies.map(movie => (
+          <p key={movie.id}> {movie.name} </p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
